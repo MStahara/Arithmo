22884d16a647400bfc8f640857cf538dee6babeb
MStahara <mitchell.stahara2006@gmail.com>
2023-05-04 01:06:39 +0000

    Clang Format

--- Arithmo.c
+++ Arithmo.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-int main(){
-	char operation;
-	float num1, num2, result;
-	int exit = 0;
+int main() {
+  char operation;
+  float num1, num2, result;
+  int exit = 0;
 
-		
-	printf("Hello, I am Arithmo,  please enter your equation:\n"); // Arithmo Introduction
+  printf("Hello, I am Arithmo,  please enter your equation:\n"); // Arithmo
+                                                                 // Introduction
 
-		while (!exit) {
-			scanf("%f %c %f", &num1, &operation, &num2);
-				if (operation == '+'){
-				result = num1 + num2;
-			        } else if (operation == '-'){
-				result = num1 - num2;
-				} else if (operation == '*'){
-				result = num1 * num2;
-				} else if (operation == '/'){
-				result = num1 / num2;
-				} 
-			printf("The result is %f\n", result);
-			}
+  while (!exit) {
+    scanf("%f %c %f", &num1, &operation, &num2);
+    if (operation == '+') {
+      result = num1 + num2;
+    } else if (operation == '-') {
+      result = num1 - num2;
+    } else if (operation == '*') {
+      result = num1 * num2;
+    } else if (operation == '/') {
+      result = num1 / num2;
+    }
+    printf("The result is %f\n", result);
+  }
 
-	return(0);
+  return (0);
 }
