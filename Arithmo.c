899ff8b354a17f3edb652f89e09a6a061047adeb
MStahara <mitchell.stahara2006@gmail.com>
2023-05-05 02:13:12 +0000

    ArithmoV1.2.4

--- Arithmo.c
+++ Arithmo.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
+float perform_operation(float result, float num1, char operation, float num2) {
+  if (operation == '+') {
+    result = num1 + num2;
+  } else if (operation == '-') {
+    result = num1 - num2;
+  } else if (operation == '*') {
+    result = num1 * num2;
+  } else if (operation == '/') {
+    result = num1 / num2;
+  } else if (operation == '^') {
+    printf("Keh?\n");
+  }
+  printf("the result is %f\n", result);
+  return 0;
+}
+
 int main() {
   char operation;
   float num1, num2, result, check;
   int exit = 0;
 
-  printf("Hello, I am Arithmo,  please enter your equation:\n"); // Arithmo
-                                                                 // Introduction
+  printf("Hello, I am Arithmo,  please enter your equation:\n");
 
   while (!exit) {
     check = scanf("%f %c %f", &num1, &operation, &num2);
-	if (check == 3) {
-      if (operation == '+') {
-      result = num1 + num2;
-    } else if (operation == '-') {
-      result = num1 - num2;
-    } else if (operation == '*') {
-      result = num1 * num2;
-    } else if (operation == '/') {
-      result = num1 / num2;
-    } else if (operation == '^') {
-	printf("Keh?\n");
-		break;
-    }
-    printf("The result is %f\n", result);
-    }
-	else {
-	 printf("Something went wrong!\n");
-		break;
-	}
-}
-	return (0); 
+    if (check != 3) {
+      printf("Something Went Wrong!");
+      break;
+    } else
+      perform_operation(result, num1, operation, num2);
+  }
+  return 0;
 }
